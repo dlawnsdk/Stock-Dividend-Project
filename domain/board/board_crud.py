bedf74767bea0a4970ce87e319b5e0b8587fb704
Juna Im <dlawnsdk04@naver.com>
2023-02-21 00:41:39 +0900

    Check Per column not Object

--- domain/board/board_crud.py
+++ domain/board/board_crud.py
@@ -4,5 +4,5 @@ from sqlalchemy import select
 
 
 async def get_board_list(db: Session):
-    board_list = await db.execute(select(Board).order_by(Board.create_date.desc()))
+    board_list = await db.execute(select(Board.id, Board.subject, Board.content, Board.create_date).order_by(Board.create_date.desc()))
     return board_list.all()
