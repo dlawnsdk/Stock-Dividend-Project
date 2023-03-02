8ffed06df3e6866dd693878a8a2e1bf380a4aa92
Juna Im <dlawnsdk04@naver.com>
2023-03-02 15:09:27 +0900

    Request API for Type Json and render in view page

--- domain/stock/stock_router.py
+++ domain/stock/stock_router.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 import requests
+from requests import Response
+from requests.models import Response
+resp = Response()
 
 
 router = APIRouter(prefix="/api/stock")
@@ -8,7 +11,14 @@ router = APIRouter(prefix="/api/stock")
 @router.get('/list')
 def stock_list():
     url = 'http://apis.data.go.kr/1160100/service/GetStocDiviInfoService/getDiviInfo'
-    params = {'serviceKey': 'X3EjOtLbzPsZO0sDXdwImY5+EfRe0rrb7O8XtPHRhvjzFNghyIHx+41YK8EZETVdI5qMID3aww6/KDrwAFg9fA=='}
+    params = {'serviceKey': 'X3EjOtLbzPsZO0sDXdwImY5+EfRe0rrb7O8XtPHRhvjzFNghyIHx+41YK8EZETVdI5qMID3aww6/KDrwAFg9fA==', 'resultType': 'JSON'}
 
-    test = requests.get(url, params=params)
-    return test.content
+    stock_list: Response = requests.get(url, params=params) # requests.models.response
+
+    result_length = len(stock_list.json().get('response').get('body').get('items').get('item'))
+
+    result = {}
+    for i in range(result_length):
+        result[i] = stock_list.json().get('response').get('body').get('items').get('item')[i].get('isinCdNm')
+
+    return result
--- frontend/src/routes/stock/list.svelte
+++ frontend/src/routes/stock/list.svelte
@@ -2,16 +2,17 @@
   import fastapi from "../../lib/api.js"
   import { link } from 'svelte-spa-router'
 
-  let stock_list = ''
+  let stock_list = {}
 
   fastapi('get', '/api/stock/list', {}, (json) => {
-        console.log(json)
+        console.log(json[0])
          stock_list = json
      })
 
 </script>
 
 <ul>
-    {stock_list}
+     {#each Object.entries(stock_list) as [key, value], idx}
+         <li>>{idx+1}: {value}</li> <!-- 키와 값 출력 -->
+    {/each}
 </ul>
-<div class="my-3"><a use:link href="/board/edit/new" class="btn btn-sm btn-outline-secondary">등록</a></div>
