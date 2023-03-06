617d02bdcccb2c37c9c32c29801a009d783ef526
Juna Im <dlawnsdk04@naver.com>
2023-03-06 21:50:54 +0900

    Search a Compony for stock

--- domain/stock/stock_router.py
+++ domain/stock/stock_router.py
@@ -9,17 +9,17 @@ router = APIRouter(prefix="/api/stock")
 
 
 @router.get('/list')
-def stock_list():
+def stock_list(keyword: str = ''):
     url = 'http://apis.data.go.kr/1160100/service/GetStocDiviInfoService/getDiviInfo'
-    params = {'serviceKey': 'X3EjOtLbzPsZO0sDXdwImY5+EfRe0rrb7O8XtPHRhvjzFNghyIHx+41YK8EZETVdI5qMID3aww6/KDrwAFg9fA==', 'pageNo' : '1', 'numOfRows' : '1', 'resultType' : 'json', 'stckIssuCmpyNm' : '하이닉스' }
+    params = {'serviceKey': 'X3EjOtLbzPsZO0sDXdwImY5+EfRe0rrb7O8XtPHRhvjzFNghyIHx+41YK8EZETVdI5qMID3aww6/KDrwAFg9fA==', 'pageNo' : '1', 'numOfRows' : '1', 'resultType' : 'json', 'stckIssuCmpyNm' : keyword }
 
     stock_list: Response = requests.get(url, params=params) # requests.models.response
 
     result_length = len(stock_list.json().get('response').get('body').get('items').get('item'))
 
     result = {}
+
     for i in range(result_length):
         result[i] = stock_list.json().get('response').get('body').get('items').get('item')[i]
-        print(result[i])
 
     return result
--- frontend/src/routes/stock/list.svelte
+++ frontend/src/routes/stock/list.svelte
@@ -4,15 +4,23 @@
 
   let stock_list = {}
 
-  fastapi('get', '/api/stock/list', {}, (json) => {
-         stock_list = json[0]
-        console.log(stock_list)
-     })
+
+
+  let search = () => {
+      let params = {
+          keyword: document.getElementById("keyword").value
+      }
+      fastapi('get', '/api/stock/list', params, (json) => {
+          if(json[0] != undefined){
+              stock_list = json[0]
+          }
+      })
+  }
 
 </script>
 
-<ul>
+<input type="text" id="keyword"><button on:click={search}>검색</button>
+
      {#each Object.entries(stock_list) as [key, value], idx}
-         <li>>{key}: {value}</li> <!-- 키와 값 출력 -->
+         <div>{key}: {value}</div> <!-- 키와 값 출력 -->
     {/each}
-</ul>
