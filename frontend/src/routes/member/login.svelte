ae2534cdbc951d78f408afb666c190d0cad96e8e
Juna Im <dlawnsdk04@naver.com>
2023-03-21 23:33:07 +0900

    Implementing login function with KakaoAPI

--- frontend/src/routes/member/login.svelte
+++ frontend/src/routes/member/login.svelte
@@ -1,16 +1,10 @@
-<script>
-  import fastapi from "../../lib/api.js"
-  import { link } from 'svelte-spa-router'
 
-  let board_list = []
-  const REST_API_KEY = 'eec0b007a1d0f71c05c99e3130b08d0b'
-  const REDIRECT_URI = 'http://localhost:8000/auth/kakao/callback'
 
-  // fastapi('get', '/api/member/agreeList', {}, (json) => {
-  //        board_list = json.board_list
-  //    })
-
-  // location.replace('https://accounts.kakao.com/login/?continue=https%3A%2F%2Fkauth.kakao.com%2Foauth%2Fauthorize%3Fresponse_type%3Dcode%26redirect_uri%3Dhttp%253A%252F%252Flocalhost%253A5173%252Fapi%252Fmember%252FtryLogin%26through_account%3Dtrue%26client_id%3Deec0b007a1d0f71c05c99e3130b08d0b#login')
-
-  location.replace('https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=eec0b007a1d0f71c05c99e3130b08d0b&redirect_uri=http://localhost:8000/auth/kakao/callback')
+<script src="https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js" integrity="sha384-dpu02ieKC6NUeKFoGMOKz6102CLEWi9+5RQjWSV0ikYSFFd8M3Wp2reIcquJOemx" crossorigin="anonymous">
+  import KakaoLogin from "@svkakao/svelte-kakao-login";
 </script>
+
+
+<KakaoLogin key="21a44baa64e55564eb933d9bf7046417" />
+
+
